1ff21114e61284b9a5580b9341227274eb45a84c
Ovsyankinds <ovsyankinds@gmail.com>
2017-08-29 20:24:32 +0300

    1 commit

---
+++ myRealm/Model.swift
@@ -0,0 +1,14 @@
+//
+//  Model.swift
+//  myRealm
+//
+//  Created by Ovsyankinds on 8/29/17.
+//  Copyright © 2017 Ovsyankinds. All rights reserved.
+//
+
+import UIKit
+import RealmSwift
+
+class Model: Object {
+    dynamic var name = ""
+}
--- myRealm/ViewController.swift
+++ myRealm/ViewController.swift
@@ -7,10 +7,46 @@
 //
 
 import UIKit
+import RealmSwift
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var text: UITextField!
+    
+    @IBAction func add(){
+    
+        var myModel = Model()
+        myModel.name = text.text!
+        
+        let realm = try! Realm()
 
+        
+        try! realm.write {
+            realm.add(myModel)
+        }
+
+        
+    }
+    
+    @IBAction func show(){
+        let realm = try! Realm()
+        let result = realm.objects(Model.self)
+        
+        for row in result{
+            print(row.name)
+        }
+        //print(result[0].name)
+        
+    }
+    
+    //let realm = try! Realm()
+    var result: Results<Model>?
+    
+    
     override func viewDidLoad() {
+        
+        result = try! Realm().objects(Model.self)
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
@@ -22,4 +58,26 @@ class ViewController: UIViewController {
 
 
 }
+
+
+extension ViewController: UITableViewDataSource{
+    @available(iOS 2.0, *)
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel!.text = result?[indexPath.row].name
+        return cell
+    }
+}
+
+
+
+
+
+
+
+
 
